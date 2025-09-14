653abcd98820766f725cc74891ae272ebebf86fe
nixuris <nixuriss@proton.me>
2025-09-14 15:28:21 +0700

    versioning

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "serein/cmd"
+import (
+	"fmt"
+	"os"
+	"serein/cmd"
+)
+
+var version = "dev"
 
 func main() {
+	if len(os.Args) > 1 && (os.Args[1] == "-v" || os.Args[1] == "--version") {
+		fmt.Printf("serein version: %s\n", version)
+		return
+	}
 	cmd.Execute()
 }
